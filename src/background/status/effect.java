1da53c82edc094916f7efa3acc599a4be6054b5c
einnuj <lilshortykidazn1@gmail.com>
2014-12-11 22:07:34 -0500

    No idea
    But Intellij is acting fussy about it

--- src/background/status/effect.java
+++ src/background/status/effect.java
@@ -1,7 +1,22 @@
 package background.status;
 
+import background.EffectName;
+import background.EffectType;
+
 /**
+ * A simple abstract class that details any type of ailments possible in a fight.
+ *
  * Created by Junnie on 12/10/2014.
  */
-public class effect {
+public abstract class Effect {
+    private EffectName name;
+    private EffectType type;
+
+    /**
+     * This method exists to describe what should happen to an entity Character upon successful
+     * infliction of said status Effect.
+     *
+     * @param c     The target to whom the affliction is applied.
+     */
+    public abstract void inflict(Character c);
 }
