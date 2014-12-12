0e79ab6f11773d926b0c77ea5d4b227335f3c3b1
einnuj <lilshortykidazn1@gmail.com>
2014-12-12 01:52:35 -0500

    Fleshed out a little bit more of the Fight logic, adding a couple methods.
    Fight can be checked for completion; started some methods for basic input prints for fights.

---
+++ src/background/actions/TargetType.java
@@ -0,0 +1,10 @@
+package background.actions;
+
+/**
+ * This enum represents which targetting system an Action undertakes.
+ *
+ * Created by Junnie on 12/11/2014.
+ */
+public enum TargetType {
+    SELF, ALLY, ALLIES, PARTY, ENEMY, ENEMIES, ENEMY_PARTY
+}
--- src/simulator/Fight.java
+++ src/simulator/Fight.java
@@ -14,7 +14,24 @@ public class Fight {
     private Character[] computer;
     private boolean turn;
 
+
     /** Getters & Setters **/
+
+    public Character[] getControlled() {
+        return controlled;
+    }
+
+    public void setControlled(Character[] controlled) {
+        this.controlled = controlled;
+    }
+
+    public Character[] getComputer() {
+        return computer;
+    }
+
+    public void setComputer(Character[] computer) {
+        this.computer = computer;
+    }
 
     public boolean isTurn() {
         return turn;
@@ -36,6 +53,14 @@ public class Fight {
         turn = false;
     }
 
+    // Menu Methods
+
+    /**
+     * This method returns a String array populated with the names of possible Melee Options
+     *
+     * @param c     The Character from which to generate melee options
+     * @return      A String populated array representing melee options
+     */
     public ArrayList<String> generateMeleeOptions(Character c) {
         ArrayList<String> list = new ArrayList<String>();
 
@@ -46,6 +71,12 @@ public class Fight {
         return list;
     }
 
+    /**
+     * This method returns a String array populated with the names of possible Magic Options
+     *
+     * @param c     The Character from which to generate magic options
+     * @return      A String populated array representing magic options
+     */
     public ArrayList<String> generateMagicOptions(Character c) {
         ArrayList<String> list = new ArrayList<String>();
 
@@ -54,5 +85,112 @@ public class Fight {
         }
 
         return list;
+    }
+
+    /**
+     * Prints the default battle menu
+     */
+    public void printBattleMenu() {
+        System.out.printf("Choose an option:\nFight\tItems\tEvade\tRun\n");
+        return;
+    }
+
+    /**
+     * Prints the Fight Options in a fight
+     */
+    public void printFightMenu() {
+        System.out.printf("Fight:\nMelee\tMagic\n");
+        return;
+    }
+
+    /**
+     * Prints the Melee Options available to a Character
+     *
+     * @param c     Character's Melee Options to print
+     */
+    public void printMeleeMenu(Character c) {
+        int i = 0;
+
+        for(String s : generateMeleeOptions(c)) {
+            if(i % 3 == 0) {
+                System.out.printf("\n");
+                i = 0;
+            }
+            System.out.printf("%s\t", s);
+            i++;
+        }
+
+        return;
+    }
+
+    /**
+     * Prints the Magic Options available to a Character
+     *
+     * @param c     Character's Magic Options to print
+     */
+    public void printMagicMenu(Character c) {
+        int i = 0;
+
+        for(String s : generateMagicOptions(c)) {
+            if(i % 3 == 0) {
+                System.out.printf("\n");
+                i = 0;
+            }
+            System.out.printf("%s\t", s);
+            i++;
+        }
+
+        return;
+    }
+
+    /**
+     * A method that calculated whether or not a Fight's combatants on either side is wholly
+     * defeated.
+     *
+     * @return      -1 if Player loses, 1 if Computer is defeated, 0 otherwise.
+     */
+    public int isFightOver() {
+        boolean computerDefeated = false;
+        int i = 0;
+
+        /*
+        Loops through the Player's team and increments per Character defeated
+         */
+        for(Character c : controlled) {
+            if(c.getStats().gethP() == 0) {
+                i++;
+            }
+        }
+
+        /*
+        Returns -1 if all Player's Characters are defeated
+         */
+        if (i == controlled.length) {
+            return -1;
+        }
+
+        // Resets the defeated counter
+        i = 0;
+
+        /*
+        Loops through the Computer's team and increments per Character defeated
+         */
+        for(Character c : computer) {
+            if(c.getStats().gethP() == 0) {
+                i++;
+            }
+        }
+
+        /*
+        Returns 1 if all Computer's Characters are defeated
+         */
+        if (i == computer.length) {
+            return 1;
+        }
+
+        /*
+        Returns 0 if not all of one team is defeated
+         */
+        return 0;
     }
 }
